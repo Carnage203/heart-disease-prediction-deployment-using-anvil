9ae50ffaff35c459aab2f395dc630d118cc9e471
sohamkvmandal@gmail.com <sohamkvmandal@gmail.com>
2024-04-20 05:46:11 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import plotly.graph_obects as go
+import anvil.server
+
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +10,45 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def primary_color_1_click(self, **event_args):
+
+    """This method is called when the button is clicked"""
+    diabetes=anvil.server.call('CHECK',
+                              self.Age.text,
+                              self.Sex.text,
+                              self.Chest_pain_type.text,
+                              self.BP.text,
+                              self.Cholestrol.text,
+                              self.FBS_Over_120.text,
+                              self.EKG_Results.text,
+                              self.Exercise_angina.text,
+                              self.ST_depression.text,
+                              self.Slope_of_ST.text,
+                              self.Number_of_vessels_fluro.text,
+                              self.Thallium.text,
+                              )
+    if Heart_Disease=="true":
+      self.Heart_Disease.visible=True
+      self.Heart_Disease.text="You have Heart Disease"
+    else:
+      self.Heart_Disease.visible=True
+      self.Heart_Disease.text="You have no Heart Disease"
+
+  def plot_1_click(self, points, **event_args):
+    """This method is called when a data point is clicked."""
+    pass
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+
+
+  
+
+  
